19b161121787df09658b91fa50c1f64c37406417
Benjamin Bowes <bbowes999@gmail.com>
2018-01-29 18:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 function nestedTarget(){
-  return document.
+  return document.querySelector('#nested .target')
   
 }
