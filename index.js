df812a39228ce117635220e8340358742420c970
Benjamin Bowes <bbowes999@gmail.com>
2018-01-29 18:55:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ function increaseRankBy(n) {
     }    
 }
 }
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+}
